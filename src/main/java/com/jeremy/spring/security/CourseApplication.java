2ceda1d1eb8c44cc34abe29f9e68ac7f31fe266b
tuyenluong <76527250+tuyenluong@users.noreply.github.com>
2025-03-11 22:59:19 +0700

    First commit

---
+++ src/main/java/com/jeremy/spring/security/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.jeremy.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
