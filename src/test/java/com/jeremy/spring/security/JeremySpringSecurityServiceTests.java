75936e2100518be8bb3b6e0901ac2286d37c1a85
tuyenluong <76527250+tuyenluong@users.noreply.github.com>
2025-03-12 00:56:07 +0700

    [Security 10-22] Spring Security

--- src/test/java/com/jeremy/spring/security/JeremySpringSecurityServiceTests.java
+++ src/test/java/com/jeremy/spring/security/JeremySpringSecurityServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CourseApplicationTests {
+class JeremySpringSecurityServiceTests {
 
 	@Test
 	void contextLoads() {
